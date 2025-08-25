8b92c0cba6b3fb408d6e97a599d7f327887be179
Lucas Hot <lucashot@Mr-Hot-MacBook.mshome.net>
2025-08-25 11:28:11 -0300

    finalizacao do frontend e correcao de erros

--- frontend/crud-policiais-frontend/src/app/app.component.ts
+++ frontend/crud-policiais-frontend/src/app/app.component.ts
@@ -10,3 +10,15 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'crud-policiais-frontend';
 }
+
+
+
+{
+  "id": 2,
+  "rg_civil": "34567890",
+  "rg_militar": "09876543",
+  "cpf": "34567890123",
+  "data_nascimento": "1988-12-15T00:00:00.000Z",
+  "matricula": "$2a$10$2xyz..."
+}
+]
